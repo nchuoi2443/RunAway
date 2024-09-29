54174879eea88bf901e614f1572e99f092fca3d1
nchuoi2443 <nguyenminhngoc24042003@gmail.com>
2024-09-30 02:53:12 +0700

    + complete range enemy

--- Assets/Scrips/Enemies/RangeEnemy.cs
+++ Assets/Scrips/Enemies/RangeEnemy.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
-public class RangeEnemy : MonoBehaviour
+public class RangeEnemy : Enemy
 {
-    private GameObject bulletPrefab;
+    [SerializeField] private GameObject bulletPrefab;
     [Range(5f, 10f)]
     [SerializeField] private float attackRange = 5f;
     [Range(0.5f, 3f)]
@@ -13,5 +14,49 @@ public class RangeEnemy : MonoBehaviour
     private float fireRateTimer;
     [SerializeField] private Transform firePos;
 
+    private void Awake()
+    {
+        fireRateTimer = fireRate;
+    }
+    private void Update()
+    {
+        if (Target!)
+        {
+            getTarget();
+        }
+        if (Target == null)
+        {
+            Debug.Log("we got nothing");
+        }
+        else
+        {
+            rotateTowardTarget();
+        }
+    }
 
+    //add enemy with max range attack and fire range have the same value, and will update after done that!
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(Target.position, transform.position) > attackRange)
+        {
+            //move forward
+            Rb.velocity = transform.up * Speed;
+        } else
+        {
+            
+            if (fireRateTimer <= 0)
+            {
+                Shoot();
+                fireRateTimer = fireRate;
+            } else
+            {
+                fireRateTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+    }
 }
