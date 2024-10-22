6dfe11bc27b12e5ae87fd9475dedc7c29d84bdc0
nchuoi2443 <nguyenminhngoc24042003@gmail.com>
2024-10-23 01:20:22 +0700

    + early UI shop to buy stuff

---
+++ Assets/Scrips/UI/UIShop.cs
@@ -0,0 +1,54 @@
+﻿using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIShop : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
+    {
+        container = transform.Find("Container");
+        shopItemTemplate = container.Find("ShopItemTemplate");
+
+        shopItemTemplate.gameObject.SetActive(false);
+        
+    }
+
+    private void Start()
+    {
+        CreateShopItem(Items.ItemType.MaxHealth, Items.GetSprite(Items.ItemType.MaxHealth), "Max Health", Items.GetCost(Items.ItemType.MaxHealth), 0);
+        CreateShopItem(Items.ItemType.MaxStamina, Items.GetSprite(Items.ItemType.MaxStamina), "Max Stamina", Items.GetCost(Items.ItemType.MaxStamina), 1);
+        CreateShopItem(Items.ItemType.Damage, Items.GetSprite(Items.ItemType.Damage), "Damage", Items.GetCost(Items.ItemType.Damage), 2);
+        CreateShopItem(Items.ItemType.Sword, Items.GetSprite(Items.ItemType.Sword), "Sword", Items.GetCost(Items.ItemType.Sword), 3);
+        CreateShopItem(Items.ItemType.Shield, Items.GetSprite(Items.ItemType.Shield), "Shield", Items.GetCost(Items.ItemType.Shield), 4);
+        CreateShopItem(Items.ItemType.Bow, Items.GetSprite(Items.ItemType.Bow), "Bow", Items.GetCost(Items.ItemType.Bow), 5);
+        CreateShopItem(Items.ItemType.Staff, Items.GetSprite(Items.ItemType.Staff), "Staff", Items.GetCost(Items.ItemType.Staff), 6);
+
+    }
+
+    private void CreateShopItem(Items.ItemType itemType, Sprite itemSprite, string itemName, int itemPrice, int positionIndex)
+    {
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+        float shopItemHeight = 100f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+        shopItemTransform.Find("PriceText").GetComponent<TextMeshProUGUI>().SetText(itemPrice.ToString());
+        shopItemTransform.Find("itemImage").GetComponent<Image>().sprite = itemSprite;
+        shopItemTransform.Find("NameText").GetComponent<TextMeshProUGUI>().SetText(itemName);
+
+        shopItemTransform.GetComponent<Button_UI>().ClickFunc = () =>
+        {
+            TryBuyItem(itemType);
+        };
+    }
+
+    private void TryBuyItem(Items.ItemType itemType)
+    {
+        
+    }
+}
